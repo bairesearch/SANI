c4b11f05c6b0d8a7314ddaa57dbbd3a0cdf48183
Richard Baxter <richard.baxter@baxterai.com>
2020-08-05 18:03:27 +1000

    SANI 1f3m - remove not from wordlistsAdverb.txt SANI 1f3m - add not from wordlistsUnknown.txt (to prevent it from being interpreted as a wildcard/noun) SANI 1f3m - create auxiliarySequence3/auxiliarySequenceHaving3/auxiliarySequenceBeing3/auxiliarySequenceDoing3 to prevent auxiliary sequences from starting with 'not'

--- SANI/GIAtxtRelTranslatorRulesGroupClass.cpp
+++ SANI/GIAtxtRelTranslatorRulesGroupClass.cpp
@@ -26,7 +26,7 @@
  * File Name: GIAtxtRelTranslatorRulesGroupClass.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2018 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 3f3l 10-April-2018
+ * Project Version: 3f3m 10-April-2018
  * Requirements: requires plain text file
  * Description: Textual Relation Translator Rules
  * /
