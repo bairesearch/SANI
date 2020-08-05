2a239659b3aebba114346f0292af259ceda31f96
Richard Baxter <richard.baxter@baxterai.com>
2020-08-05 18:03:08 +1000

    SANI 1f1u - partially revert change introduced @GIA3f1q; remove "update subReferenceSetsAuxiliaryDelimiterAndObject:actionReverse to use this instead of auxiliarySequence" from "create auxiliarySequenceBeingOrHaving and update subReferenceSetsAuxiliaryDelimiterAndObject:actionReverse to use this instead of auxiliarySequence, and check actionReverse before action" SANI 1f1u - revert change introduced @GIA3f1s; "[CHECKTHIS] update subReferenceSetsAuxiliaryDelimiterAndObject:definition and subReferenceSetsDelimiterAndObject:definition to use subReferenceSetsSubject instead of subReferenceSetsSpecialPossessiveOrDeterminerThingEnforced"

--- SANI/GIAtxtRelTranslatorRules.hpp
+++ SANI/GIAtxtRelTranslatorRules.hpp
@@ -26,7 +26,7 @@
  * File Name: GIAtxtRelTranslatorRules.hpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2018 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 3f1t 22-February-2018
+ * Project Version: 3f1u 22-February-2018
  * Requirements: requires plain text file
  * Description: Textual Relation Translator Rules
  * /
