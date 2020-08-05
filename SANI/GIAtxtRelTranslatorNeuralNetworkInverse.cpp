2e0bd99fa807df494134a7aaf8350c75b432625f
Richard Baxter <richard.baxter@baxterai.com>
2020-08-05 18:27:46 +1000

    SANI 1i2c - update logicReferenceSets:conjunction1/logicReferenceSets:conjunction2 with semanticRelationReturnEntity (semanticRelationIndexType="delimiter" semanticRelationReturnEntity="true")

--- SANI/GIAtxtRelTranslatorNeuralNetworkInverse.cpp
+++ SANI/GIAtxtRelTranslatorNeuralNetworkInverse.cpp
@@ -26,7 +26,7 @@
  * File Name: GIAtxtRelTranslatorNeuralNetworkInverse.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2019 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 3i2b 27-May-2019
+ * Project Version: 3i2c 27-May-2019
  * Requirements: requires plain text file
  * Description: Textual Relation Translator Neural Network Inverse
  * /
