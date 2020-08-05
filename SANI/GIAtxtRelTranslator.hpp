7bedfcb1b70d700a129e82a1addddf81b44ab174
Richard Baxter <richard.baxter@baxterai.com>
2020-08-05 18:03:16 +1000

    SANI 1f2k - [CHECKTHIS] upgrade logicReferenceSets preposition1/preposition2 to detect all prepositions (not just those from tokens)? SANI 1f2k - update conditionAndConclusion2 to support an intermediate comma (optional)

--- SANI/GIAtxtRelTranslator.hpp
+++ SANI/GIAtxtRelTranslator.hpp
@@ -26,7 +26,7 @@
  * File Name: GIAtxtRelTranslator.hpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2018 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 3f2j 04-April-2018
+ * Project Version: 3f2k 04-April-2018
  * Requirements: requires plain text file
  * Description: Textual relation translator
  * /
