1bbba478e9d2820bcc933473119dbf9b9e68c42b
Richard Baxter <richard.baxter@baxterai.com>
2020-08-05 18:03:46 +1000

    SANI 1f10f - create referenceSets:definition3

--- SANI/GIAtxtRelTranslator.hpp
+++ SANI/GIAtxtRelTranslator.hpp
@@ -26,7 +26,7 @@
  * File Name: GIAtxtRelTranslator.hpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2018 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 3f10e 19-April-2018
+ * Project Version: 3f10f 19-April-2018
  * Requirements: requires plain text file
  * Description: Textual relation translator
  * /
