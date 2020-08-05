63e3bc3034f0cf61316655e743985fc996f0a347
Richard Baxter <richard.baxter@baxterai.com>
2020-08-05 18:03:00 +1000

    SANI 1f1g - update wordlistDate.txt to set all months/days upper case, and update GIAtranslatorGrammar to set all dates to properNouns (such that lower case lemmas are not created)

--- SANI/GIAglobalDefs.hpp
+++ SANI/GIAglobalDefs.hpp
@@ -26,7 +26,7 @@
  * File Name: GIAglobalsDefs.hpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2018 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 3f1f 22-February-2018
+ * Project Version: 3f1g 22-February-2018
  * Requirements: requires text parsed by NLP Parser (eg Relex; available in .CFF format <relations>)
  * Description: GIA specific global definitions
  * /
--- SANI/GIAtxtRelTranslatorRulesComponentClass.cpp
+++ SANI/GIAtxtRelTranslatorRulesComponentClass.cpp
@@ -26,7 +26,7 @@
  * File Name: GIAtxtRelTranslatorRulesComponentClass.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2018 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 3f1f 22-February-2018
+ * Project Version: 3f1g 22-February-2018
  * Requirements: requires plain text file
  * Description: Textual Relation Translator Rules
  * /
