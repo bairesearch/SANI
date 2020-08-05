0ebb793dee04f2cde847c6c5408af34d09316273
Richard Baxter <richard.baxter@baxterai.com>
2020-08-05 18:03:14 +1000

    SANI 1f2g - support quantities - update subReferenceSetsSubject to support quantityNoun SANI 1f2g - ensure that quantities are not concepts

--- SANI/GIAtxtRelTranslator.cpp
+++ SANI/GIAtxtRelTranslator.cpp
@@ -26,7 +26,7 @@
  * File Name: GIAtxtRelTranslator.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2018 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 3f2f 04-April-2018
+ * Project Version: 3f2g 04-April-2018
  * Requirements: requires plain text file
  * Description: Textual relation translator
  * /
